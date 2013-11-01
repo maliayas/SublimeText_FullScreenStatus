2ef9fb93d4e277ead35c563a898d0fc4590872dc
Ali Ayas <dev@maliayas.com>
2013-11-01 15:44:26 +0200

    Use plugin_loaded() for initial work

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import sublime, sublime_plugin, os
 
+plugin_installed_marker = None
+full_screen_marker      = None
+distraction_free_marker = None
 
-# Marker files.
-plugin_installed_marker = sublime.cache_path() + os.sep + ".fs-status-installed"
-full_screen_marker      = sublime.cache_path() + os.sep + ".on-full-screen"
-distraction_free_marker = sublime.cache_path() + os.sep + ".on-distraction-free"
+def plugin_loaded():
+    global plugin_installed_marker, full_screen_marker, distraction_free_marker
 
-# Put a marker that indicates this plugin is installed. Otherwise, absence of
-# `full_screen_marker` and `distraction_free_marker` wouldn't have a meaning.
-open(plugin_installed_marker, "w+")
+    # Marker files.
+    plugin_installed_marker = sublime.cache_path() + os.sep + ".fs-status-installed"
+    full_screen_marker      = sublime.cache_path() + os.sep + ".on-full-screen"
+    distraction_free_marker = sublime.cache_path() + os.sep + ".on-distraction-free"
 
-# Remove any markers on startup that may be left from the previous session. Because
-# ST can't start on full screen or distraction free mode.
-try:
-    os.remove(full_screen_marker)
-    os.remove(distraction_free_marker)
-except:
-    pass
+    # Put a marker that indicates this plugin is installed. Otherwise, absence of
+    # `full_screen_marker` and `distraction_free_marker` wouldn't have a meaning.
+    open(plugin_installed_marker, "w+")
+
+    # Remove any markers on startup that may be left from the previous session. Because
+    # ST can't start on full screen or distraction free mode.
+    try:
+        os.remove(full_screen_marker)
+        os.remove(distraction_free_marker)
+    except:
+        pass
 
 class ToggleFullScreenWrapperCommand(sublime_plugin.WindowCommand):
     def run(self):
